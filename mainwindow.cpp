36199a51a17a1d9d3b9a1a51628c654863935e9d
MathewLister <listermathew0@gmail.com>
2020-03-16 15:05:46 -0700

    Added very stupid AI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,7 +17,8 @@ MainWindow::MainWindow(QWidget *parent)
     // Declaring mainMap and mainPlayer.
     GameMap mainMap = GameMap(5, 5);
     GameObject* mainPlayer = mainMap.create<Player>(0, 0, true);
-    mainMap.create<Goblin>(1, 1);
+    GameObject* goblin1 = mainMap.create<Goblin>(1, 1);
+    GameObject* goblin2 = mainMap.create<GoblinBoss>(1,2);
     //mainMap.create<Spike>(1, 0);
     //mainMap.create<Tombstone>(1,0);
     //mainMap.create<Wall>(1, 1);
@@ -33,11 +34,26 @@ MainWindow::MainWindow(QWidget *parent)
     qDebug().noquote() << mainMap.print();
 
     mainPlayer->move(0, 1);
+
     qDebug().noquote() << mainMap.print();
+
+    while(mainPlayer->health() != 0.0)
+    {
+        mainPlayer->move(irandom(5), irandom(5));
+        goblin2->move(irandom(5), irandom(5));
+        goblin2->move(irandom(5), irandom(5));
+        qDebug().noquote() << mainMap.print();
+
+    }
+
+    if(mainPlayer->health() == 0.0){
+        qDebug() << "DEAD BRO";
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
